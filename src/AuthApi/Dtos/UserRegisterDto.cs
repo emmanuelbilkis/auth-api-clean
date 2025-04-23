a3892acb4e882373a2b816196a61654ef8e234e9
emmanuelbilkis <emmanuelbilkis@gmail.com>
2025-04-23 16:11:03 -0300

    add UserController and add UserRegistrationDto

---
+++ src/AuthApi/Dtos/UserRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace AuthApi.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string EmailConfirmed { get; set; }  
+        public string Password  { get; set; }
+        public string PasswordConfirmed { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
