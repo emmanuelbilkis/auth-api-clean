e85f3f13e29a41c3597aa5c1f1d5850d4e7399ff
emmanuelbilkis <emmanuelbilkis@gmail.com>
2025-05-15 11:02:05 -0300

    agrego repositorio de token

---
+++ src/AuthApi/Repository/TokenRepository.cs
@@ -0,0 +1,33 @@
+﻿using AuthApi.Data;
+using AuthApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace AuthApi.Repository
+{
+    public class TokenRepository
+    {
+        private readonly AppDbContext _context;
+        private readonly ILogger<TokenRepository> _logger;
+
+        public TokenRepository(AppDbContext context, ILogger<TokenRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<ActivationTokenModel> AddToken (ActivationTokenModel activationTokenModel)
+        {
+            await _context.ActivationTokens.AddAsync (activationTokenModel);
+            await _context.SaveChangesAsync(); 
+            return activationTokenModel;    
+        }
+
+        public async Task<UserModel> Register(UserModel newUser)
+        {
+            await _context.Users.AddAsync(newUser);
+            await _context.SaveChangesAsync();
+            return newUser;
+        }
+    }
+}
