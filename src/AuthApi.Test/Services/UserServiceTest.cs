f4eedd0b0288a5c80015282945995195660d97d8
emmanuelbilkis <emmanuelbilkis@gmail.com>
2025-05-21 20:39:26 -0300

    creo proyecto test

---
+++ src/AuthApi.Test/Services/UserServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using AuthApi.Interfaces.IRepository;
+using AuthApi.Services.User;
+using Moq;
+using NUnit.Framework;
+
+namespace AuthApi.Test.Services
+{
+    [TestFixture] // 
+    public class UserServiceTest
+    {
+        private Mock<IUserRepository> _mockRepository;
+        private UserService _userService;
+
+
+    }
+}
